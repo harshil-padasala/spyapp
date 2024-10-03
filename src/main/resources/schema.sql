924b2a202d50ab6f3fabd93d74765cab4f40649b
harshil-padasala <harshilpadsala999@gmail.com>
2024-10-03 22:08:29 +0530

    resolve minor bug

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -11,4 +11,4 @@ INSERT INTO mission (agent, title, gadget1, gadget2)
 VALUES
     ('Johnny English', 'Rescue the Queen', 'Exploding Sigar', 'Voice Controlled Rolls Royce'),
     ('Natasha Romanova', 'Kill Iron Man', 'Armored Suit', 'Industructible Pole'),
-    ('Austin Powers', '', 'Red, White and Blue Shaguar', 'Mini Cooper');
+    ('Austin Martins', '', 'Red, White and Blue Shaguar', 'Mini Cooper');
